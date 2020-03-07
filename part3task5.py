9bafc1ccd5f189109779bc781da09e044bf4da65
Janbolotnr <janbolotnr@gmail.com>
2020-03-08 00:17:53 +0600

    ch3part1task5

---
+++ part3task5.py
@@ -0,0 +1,37 @@
+groupmates = {
+    'Bolotov Sagun': '18.03.1997',
+    'Iskender Ismayilov': '27.12.1995',
+    'Asanov Uson': '14.07.1992'
+    }
+
+for i in groupmates:
+    print(i)
+    
+chose = str(input("Who's birthday you want look? "))
+print(chose+'s', 'birtday is', groupmates[chose])
+
+
+# for chose in groupmates:
+#     print(chose.items())
+
+
+# string = groupmates.copy()
+# print(string.items())
+# if chose == string:
+#     print(string.items())
+
+
+# string = chose.item()
+# for chose in groupmates:
+#     print(groupmates[i])
+
+# names = groupmates.keys()
+# print(names)
+
+
+# print(chose(groupmates.values()))
+
+# #print(groupmates)
+# #print(names)
+
+
